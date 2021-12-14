4df8aed57729e20d003aea5b99bb0d9016ef69fa
incogNic0 <incognico.dev@gmail.com>
2021-12-14 10:41:58 -0600

    update path for samples

--- src/assets/js/kits.js
+++ src/assets/js/kits.js
@@ -1,49 +1,28 @@
-// const kits = {
-//     rock: {
-//         path: 'assets/audio/rock/',
-//         samples: ['KICK-1', 'KICK-2', 'SNARE-1', 'HHAT-1', 'HHAT-2', 'TOM-1', 'TOM-2', 'RIDE', 'CRASH', 'TAMBORINE', 'SHAKER'],
-//     },
-//     hipHop: {
-//         path: 'assets/audio/hip-hop/',
-//         samples: ['KICK-1', 'KICK-2', 'SNARE-1', 'HHAT-1', 'HHAT-2', 'CLAP-1', 'PERC-1', 'PERC-2', 'PERC-3', 'CYMBAL-1', 'VOCAL-1'],
-//     },
-//     house: {
-//         path: 'assets/audio/house/',
-//         samples: ['KICK-1', 'SNARE-1', 'CLAP-1', 'CLAP-2', 'HHAT-1', 'HHAT-2', 'PERC-1', 'PERC-2', 'CYMBAL-1', 'STAB-1', 'TOM-1'],
-//     },
-//     techno: {
-//         path: 'assets/audio/techno/',
-//         samples: ['KICK-1', 'KICK-2', 'SNARE-1', 'SNARE-2', 'HHAT-1', 'PERC-1', 'PERC-2', 'PERC-3', 'PERC-4', 'FX-1'],
-//     },
-//     dnb: {
-//         path: 'assets/audio/dnb/',
-//         samples: ['KICK-1', 'SNARE-1', 'SNARE-2', 'HHAT-1', 'HHAT-2', 'PERC-1','BASS-1','BASS-2','BASS-3','FX-1','FX-2']
-//     }
-// }
 
 const allKits = {
     rock: {
-       path: 'https://raw.githubusercontent.com/nickn10/beat-sequencer/master/assets/audio/rock/',
+        
+       path: 'https://sequencer-test-samples.s3.us-east-2.amazonaws.com/originalKits/rock/rock/',
        samples: ['KICK-1', 'KICK-2', 'SNARE-1', 'HHAT-1', 'HHAT-2', 'TOM-1', 'TOM-2', 'RIDE', 'CRASH', 'TAMBORINE', 'SHAKER'],
        defaultTempo: 100
     },
     hipHop: {
-       path: 'https://raw.githubusercontent.com/nickn10/beat-sequencer/master/assets/audio/hip-hop/',
+       path: 'https://sequencer-test-samples.s3.us-east-2.amazonaws.com/originalKits/hip-hop/hip-hop/',
        samples: ['KICK-1', 'KICK-2', 'SNARE-1', 'HHAT-1', 'HHAT-2', 'CLAP-1', 'PERC-1', 'PERC-2', 'PERC-3', 'CYMBAL-1', 'VOCAL-1'],
        defaultTempo: 90
     },
     house: {
-       path: 'https://raw.githubusercontent.com/nickn10/beat-sequencer/master/assets/audio/house/',
+       path: 'https://sequencer-test-samples.s3.us-east-2.amazonaws.com/originalKits/house/house/',
        samples: ['KICK-1', 'SNARE-1', 'CLAP-1', 'CLAP-2', 'HHAT-1', 'HHAT-2', 'PERC-1', 'PERC-2', 'CYMBAL-1', 'STAB-1', 'TOM-1'],
        defaultTempo: 125
     },
     techno: {
-       path: 'https://raw.githubusercontent.com/nickn10/beat-sequencer/master/assets/audio/techno/',
+       path: 'https://sequencer-test-samples.s3.us-east-2.amazonaws.com/originalKits/techno/techno/',
        samples: ['KICK-1', 'KICK-2', 'SNARE-1', 'SNARE-2', 'HHAT-1', 'PERC-1', 'PERC-2', 'PERC-3', 'PERC-4', 'FX-1'],
        defaultTempo: 130
     },
     dnb: {
-       path: 'https://raw.githubusercontent.com/nickn10/beat-sequencer/master/assets/audio/dnb/',
+       path: 'https://sequencer-test-samples.s3.us-east-2.amazonaws.com/originalKits/dnb/dnb/',
        samples: ['KICK-1', 'SNARE-1', 'SNARE-2', 'HHAT-1', 'HHAT-2', 'PERC-1','BASS-1','BASS-2','BASS-3','FX-1','FX-2'],
        defaultTempo: 170
     }
